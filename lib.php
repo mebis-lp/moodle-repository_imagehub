31376c7ea950059d70c838a4bebe65ddcbf163f1
Stefan Hanauska <stefan.hanauska@csg-in.de>
2024-07-16 08:56:20 +0200

    Initial commit

---
+++ lib.php
@@ -0,0 +1,72 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * repository_imagehub plugin implementation
+ *
+ * Documentation: {@link https://moodledev.io/docs/apis/plugintypes/repository}
+ *
+ * @package    repository_imagehub
+ * @copyright  2024 ISB Bayern
+ * @author     Stefan Hanauska <stefan.hanauska@csg-in.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->dirroot . '/repository/lib.php');
+
+/**
+ * Repository repository_imagehub implementation
+ *
+ * @package    repository_imagehub
+ * @copyright  2024 ISB Bayern
+ * @author     Stefan Hanauska <stefan.hanauska@csg-in.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class repository_imagehub extends repository {
+
+    /**
+     * Given a path, and perhaps a search, get a list of files.
+     *
+     * See details on {@link http://docs.moodle.org/dev/Repository_plugins}
+     *
+     * @param string $path this parameter can a folder name, or a identification of folder
+     * @param string $page the page number of file list
+     * @return array the list of files, including meta infomation, containing the following keys
+     *           manage, url to manage url
+     *           client_id
+     *           login, login form
+     *           repo_id, active repository id
+     *           login_btn_action, the login button action
+     *           login_btn_label, the login button label
+     *           total, number of results
+     *           perpage, items per page
+     *           page
+     *           pages, total pages
+     *           issearchresult, is it a search result?
+     *           list, file list
+     *           path, current path and parent path
+     */
+    public function get_listing($path = '', $page = '') {
+        $ret = [];
+        $ret['list'] = [];
+        $ret['nosearch'] = true;
+        $ret['norefresh'] = true;
+        $ret['nologin'] = true;
+        return $ret;
+    }
+}
---
+++ version.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version information for Imagehub
+ *
+ * @package    repository_imagehub
+ * @copyright  2024 ISB Bayern
+ * @author     Stefan Hanauska <stefan.hanauska@csg-in.de>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component    = 'repository_imagehub';
+$plugin->release      = '1.0';
+$plugin->version      = 2024071500;
+$plugin->requires     = 2024042200;
+$plugin->supported    = [404, 405];
+$plugin->maturity     = MATURITY_STABLE;
